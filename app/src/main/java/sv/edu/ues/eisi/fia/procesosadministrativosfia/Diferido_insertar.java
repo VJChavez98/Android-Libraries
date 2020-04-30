c2c6bc3a25c11d1038409524c2f7c7b8d3e79b6a
Víctor <chavez98victor@gmail.com>
2020-04-30 05:27:27 -0600

    INSERTAR DIFERIDO 75%

--- app/src/main/java/sv/edu/ues/eisi/fia/procesosadministrativosfia/Diferido_insertar.java
+++ app/src/main/java/sv/edu/ues/eisi/fia/procesosadministrativosfia/Diferido_insertar.java
@@ -2,21 +2,27 @@ package sv.edu.ues.eisi.fia.procesosadministrativosfia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class Diferido_insertar extends AppCompatActivity {
 
     ControladorBase DBHelper;
     EditText editCarnet, editMateria, editGrupoTeorico, editGrupoDiscusion, editGrupoLab, editFechaEval, editHoraEval, editMotivo, editEva;
     Spinner motivos, spinTipo;
+    private int nYearIni, nMonthIni, nDayIni, sYearIni, sMonthIni, sDayIni, sHour, nHour, sMinute, nMinute;
+    static final int DATE_ID = 0, HOUR_ID=1;
+    Calendar c = Calendar.getInstance();
 
 
     @Override
@@ -25,16 +31,86 @@ public class Diferido_insertar extends AppCompatActivity {
         setContentView(R.layout.activity_diferido_insertar);
         DBHelper = new ControladorBase(getApplicationContext());
         editCarnet = (EditText) findViewById(R.id.editCarnet);
-        editMateria =  (EditText) findViewById(R.id.editAsignatura);
-        editGrupoTeorico  = (EditText) findViewById(R.id.editGrupoTeorico);
-        editGrupoDiscusion  = (EditText) findViewById(R.id.editGrupoDiscusion);
-        editGrupoLab  = (EditText) findViewById(R.id.editGrupoLab);
+        editMateria = (EditText) findViewById(R.id.editAsignatura);
+        editGrupoTeorico = (EditText) findViewById(R.id.editGrupoTeorico);
+        editGrupoDiscusion = (EditText) findViewById(R.id.editGrupoDiscusion);
+        editGrupoLab = (EditText) findViewById(R.id.editGrupoLab);
         editEva = (EditText) findViewById(R.id.editCodEva);
-        spinTipo  = (Spinner) findViewById(R.id.spinTipoEval);
-        editFechaEval  = (EditText) findViewById(R.id.editFechaRealizada);
-        editHoraEval  = (EditText) findViewById(R.id.editHoraRealizada);
+        spinTipo = (Spinner) findViewById(R.id.spinTipoEval);
+        editFechaEval = (EditText) findViewById(R.id.editFechaRealizada);
+        editHoraEval = (EditText) findViewById(R.id.editHoraRealizada);
         editMotivo = (EditText) findViewById(R.id.editMotivo);
         motivos = (Spinner) findViewById(R.id.spinMotivos);
+
+
+        sMonthIni = c.get(Calendar.MONTH);
+        sDayIni = c.get(Calendar.DAY_OF_MONTH);
+        sYearIni = c.get(Calendar.YEAR);
+        sHour = c.get(Calendar.HOUR_OF_DAY);
+        sMinute = c.get(Calendar.MINUTE);
+
+
+        editFechaEval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showDialog(DATE_ID);
+            }
+        });
+        editHoraEval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(HOUR_ID);
+            }
+        });
+    }
+    private void colocar_fecha() {
+        if (String.valueOf(nMonthIni).length() == 1 && String.valueOf(nDayIni).length() == 1){
+            editFechaEval.setText( nYearIni + "-0" + nMonthIni + "-0" + nDayIni );
+        }else if (String.valueOf(nMonthIni).length() == 1){
+            editFechaEval.setText( nYearIni + "-0" + nMonthIni + "-" + nDayIni );
+        }else if (String.valueOf(nDayIni).length() == 1) {
+            editFechaEval.setText( nYearIni + "-" + nMonthIni + "-0" + nDayIni );
+        }else {
+        editFechaEval.setText( nYearIni + "-" + nMonthIni + "-" + nDayIni );
+        }
+    }
+    private void colocarHora(){
+        if (String.valueOf(nMinute).length() == 1 && String.valueOf(nHour).length() == 1){
+            editHoraEval.setText("0"+nHour + ":0"+nMinute+":00");
+        }
+        else if (String.valueOf(nHour).length()==1){
+            editHoraEval.setText("0"+nHour + ":"+nMinute+":00");
+        }else if (String.valueOf(nMinute).length()==1){
+            editHoraEval.setText(nHour + ":0" + nMinute + ":00");
+        }else {editHoraEval.setText(nHour + ":" + nMinute + ":00");}
+    }
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            nHour = hourOfDay;
+            nMinute = minute;
+            colocarHora();
+        }
+    };
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    nYearIni = year;
+                    nMonthIni = monthOfYear;
+                    nDayIni = dayOfMonth;
+                    colocar_fecha();
+                }
+            };
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_ID:
+                return new DatePickerDialog(this, mDateSetListener, sYearIni, sMonthIni, sDayIni);
+            case HOUR_ID:
+                return new TimePickerDialog(this, mTimeSetListener,nHour,nMinute,true);
+        }
+        return null;
     }
 
 
@@ -73,6 +149,16 @@ public class Diferido_insertar extends AppCompatActivity {
     }
     public void limpiarTexto(View v) {
         editCarnet.setText("");
-
+        editCarnet.setText("");
+        editMateria.setText("");
+        editGrupoTeorico.setText("");
+        editGrupoDiscusion.setText("");
+        editGrupoLab.setText("");
+        editEva.setText("");
+        spinTipo.setSelection(0);
+        editFechaEval.setText("");
+        editHoraEval.setText("");
+        editMotivo.setText("");
+        motivos.setSelection(0);
     }
 }
