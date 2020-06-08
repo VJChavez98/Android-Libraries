e07a9ef786dd8f7b130f8ba631730ab09a6360e0
Víctor <chavez98victor@gmail.com>
2020-06-07 23:57:01 -0600

    Downgrade Gradle to 3.6.3 & 5.6.4

--- app/src/main/java/sv/edu/ues/eisi/fia/procesosadministrativosfia/Diferido_insertar.java
+++ app/src/main/java/sv/edu/ues/eisi/fia/procesosadministrativosfia/Diferido_insertar.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.InputType;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
